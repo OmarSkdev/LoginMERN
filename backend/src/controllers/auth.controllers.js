c5fd6b187257f3d2263faae2e8d928e5d19164e9
Omar <omar.sk80@gmail.com>
2024-08-28 19:57:46 -0400

    Login con token

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -35,16 +35,44 @@ export const register = async (req, res) => {
         ); */
 
         res.json({
-            id:userSaved.id,
+            id:userSaved._id,
             username:userSaved.username,
             email:userSaved.email,
             createdAt:userSaved.createdAt,
             updatedAt:userSaved.updatedAt,
         });
     } catch (error) {
-        console.log(error);                
+        res.status(500).json({ message: error.message});                
     }
     //console.log(newUser);  
     
 };
-export const login = (req, res) => res.send('login');
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const userFound = await User.findOne({ email });
+        if (!userFound) return res.status(400).json({ message: "Usuario no encontrado" });
+
+        const isMatch = await bcrypt.compare(password, userFound.password);
+        if (!isMatch) return res.status(400).json({ message: "Password incorrecta"});
+
+        const token = await createAccessToken({ id: userFound._id });
+        res.cookie("token",token);
+        res.json({
+            id:userFound._id,
+            username:userFound.username,
+            email:userFound.email,
+            createdAt:userFound.createdAt,
+            updatedAt:userFound.updatedAt,
+        });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+};
+export const logout = (req, res) => {
+    res.cookie("token", "", {
+        expires: new Date(0),
+    });
+    return res.sendStatus(200);
+}
